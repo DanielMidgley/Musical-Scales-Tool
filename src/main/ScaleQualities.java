b61ea7931e2af162c2e71fc43a764e523d646367
DanielMidgley <126402725+DanielMidgley@users.noreply.github.com>
2025-08-30 14:03:37 +0100

    Update ScaleQualities.java
    Added other modes and some other extra scale qualities

--- src/main/ScaleQualities.java
+++ src/main/ScaleQualities.java
@@ -4,10 +4,28 @@ public class ScaleQualities {
     
     private static final boolean[] maj = {true, false, true, false, true, true, false, true, false, true, false, true};
     private static final boolean[] min = {true, false, true, true, false, true, false, true, true, false, true, false};
+    private static final boolean[] dor = {true, false, true, true, false, true, false, true, false, true, true, false};
+    private static final boolean[] phr = {true, true, false, true, false, true, false, true, true, false, true, false};
+    private static final boolean[] lyd = {true, false, true, false, true, false, true, true, false, true, false, true};
+    private static final boolean[] mix = {true, false, true, false, true, true, false, true, false, true, true, false};
+    private static final boolean[] loc = {true, true, false, true, false, true, true, false, true, false, true, false};
+    private static final boolean[] dhm = {true, true, false, false, true, true, false, true, true, false, false, true};
+    private static final boolean[] mma = {true, false, true, true, false, true, false, true, false, true, false, true};
+    private static final boolean[] mmd = {true, false, true, true, false, true, false, true, true, false, true, false};
+    private static final boolean[] ham = {true, false, true, true, false, true, false, true, true, false, false, true};
     
     public static boolean[] getScaleFormat(String scaleQuality) {
         if (scaleQuality.equals("Major")) return maj;
-        if (scaleQuality.equals("Minor")) return min;
+        if (scaleQuality.equals("Natural Minor")) return min;
+        if (scaleQuality.equals("Dorian")) return dor;
+        if (scaleQuality.equals("Phrygian")) return phr;
+        if (scaleQuality.equals("Lydian")) return lyd;
+        if (scaleQuality.equals("Mixolydian")) return mix;
+        if (scaleQuality.equals("Locrian")) return loc;
+        if (scaleQuality.equals("Double Harmonic Major")) return dhm;
+        if (scaleQuality.equals("Melodic Minor (asc)")) return mma;
+        if (scaleQuality.equals("Melodic Minor (desc)")) return mmd;
+        if (scaleQuality.equals("Harmonic Minor")) return ham;
         
         boolean[] errorScaleFormat = new boolean[12];
         for (int i = 0; i < errorScaleFormat.length; i++){
@@ -17,7 +35,8 @@ public class ScaleQualities {
     }
     
     public static String[] listAllScaleQualities(){
-        String[] arr = {"Major", "Minor"};
+        String[] arr = {"Major", "Natural Minor", "Dorian", "Phrygian", "Lydian", "Mixolydian", "Locrian",
+            "Double Harmonic Major", "Melodic Minor (asc)", "Melodic Minor (desc)", "Harmonic Minor"};
         return arr;
     }
     
